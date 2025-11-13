045ebd10738ebdcc9ba3f50191ad458454afb893
siaaldo <aldo.dwicahyo@gmail.com>
2025-11-13 19:18:56 +0700

    Repush the definition

--- src/verihubs/definitions.py
+++ src/verihubs/definitions.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-
 from dagster import definitions, load_from_defs_folder
 
-
+# Define the definitions for the verihubs project
 @definitions
 def defs():
     return load_from_defs_folder(path_within_project=Path(__file__).parent)
